a0a320799fbe7d7881abd68bf374e6b01334c979
Ken Ng <ng.ken84@gmail.com>
2024-11-26 21:13:52 -0800

    Add get and patch routes

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -11,3 +11,19 @@ export const createTodo: RequestHandler = (req, res, next) => {
 
     res.status(201).json({message: 'Created the todo.', createdTodo: newTodo})
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+    res.status(200).json(TODOS);
+}
+
+export const patchTodo: RequestHandler<{id: string}> = (req, res, next) => {
+    const id = req.params.id;
+    const {text} = (req.body as {text: string});
+    const todoIndex = TODOS.findIndex(todo => todo.id === id);
+    if(todoIndex === -1) {
+        res.status(404).json({message: 'Unable to find corresponding todo item'});
+    } else {
+        TODOS[todoIndex].text = text;
+        res.status(201).json({message: 'Todo updated successfully.', updatedTodo: TODOS[todoIndex]})  
+    }
+}
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
-import { createTodo } from "../controllers/todos";
+import { createTodo, getTodos, patchTodo } from "../controllers/todos";
 
 const router = Router();
 
 router.post('/', createTodo);
 
-router.get('/');
+router.get('/', getTodos);
 
-router.patch('/:id');
+router.patch('/:id', patchTodo);
 
 router.delete('/:id');
 
